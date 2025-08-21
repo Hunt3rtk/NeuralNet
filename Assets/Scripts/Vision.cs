0b5d04d87bd585b89e72a853d316d2c5406a04c2
Hunt3rtk <htempleklassen@gmail.com>
2025-08-21 11:21:59 -0700

    vision variability

--- Assets/Scripts/Vision.cs
+++ Assets/Scripts/Vision.cs
@@ -1,18 +1,30 @@
+using Unity.VisualScripting;
 using UnityEditor;
 using UnityEngine;
 
 public class Vision : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
+    [SerializeField]
+    private float visionRange = 5f;
 
+    [SerializeField]
+    private float visionAngle = 45f;
+
+    [SerializeField]
+    [Range(1, 360)]
+    private int visionResolution = 5; // Default resolution
+
+    private int VisionResolution
+    {
+        get { return visionResolution; }
+        set
+        { visionResolution = (value % visionAngle == 0) ? value : (int)((float)value - ((float)value % visionAngle)); }  // Ensure resolution is a multiple of visionAngle;
     }
 
     // Update is called once per frame
     void Update()
     {
-        for (float i = -22.5f; i <= 22.5f; i += 5)
+        for (float i = -(visionAngle/2); i <= visionAngle/2; i += VisionResolution)
         {
             // Calculate the angle relative to the object's rotation
             float relativeAngle = i * Mathf.Deg2Rad;
@@ -22,7 +34,7 @@ public class Vision : MonoBehaviour
             Vector2 direction = new Vector2(Mathf.Cos(worldAngle), Mathf.Sin(worldAngle));
             
             // Use transform.position instead of localPosition for world space raycast
-            RaycastHit2D hit = Physics2D.Raycast(transform.position, direction, 5f * transform.localScale.x, LayerMask.GetMask("Plant"));
+            RaycastHit2D hit = Physics2D.Raycast(transform.position, direction, visionRange * transform.localScale.x, LayerMask.GetMask("Plant"));
             
             if (hit.collider != null && hit.collider.CompareTag("Plant"))
             {
@@ -37,11 +49,11 @@ public class Vision : MonoBehaviour
         float objectRotation = transform.eulerAngles.z * Mathf.Deg2Rad;
 
         
-        float rayLength = 5f * transform.localScale.x;
+        float rayLength = visionRange * transform.localScale.x;
 
         // Optional: Draw all rays for better visualization
         Gizmos.color = Color.yellow;
-        for (float i = -22.5f; i <= 22.5f; i += 5)
+        for (float i = -(visionAngle/2); i <= visionAngle/2; i += VisionResolution)
         {
             float relativeAngle = i * Mathf.Deg2Rad;
             float worldAngle = objectRotation + relativeAngle;
